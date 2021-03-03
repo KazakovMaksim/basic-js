7ca540c81a0d5a161d530cc32a44e824e397c917
KazakovMaksim <kazakovmaksimi@gmail.com>
2021-03-03 19:00:52 +0300

    add script extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,31 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let result = "";
+      
+  if (options.repeatTimes) {
+      
+      for (let i = 0; i < options.repeatTimes; i++) {
+          result += str;
+         
+         if (options.additionRepeatTimes && options.addition) {
+                  
+             for (let j = 0; j < options.additionRepeatTimes; j++) {
+                      result += options.addition;
+                 if (options.additionSeparator && (j < options.additionRepeatTimes - 1) ) {
+                   result += options.additionSeparator
+                 } else if (j < options.additionRepeatTimes - 1) {result += "|"}
+             } 
+         }
+
+        if (options.separator && (i < options.repeatTimes-1)) {
+            result += options.separator; 
+        } else if (i < options.repeatTimes-1) {
+            result += "+";   
+          }
+    
+         }
+         return result;
+  
 };
+
+}
   
