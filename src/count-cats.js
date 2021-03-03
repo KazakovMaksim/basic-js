4e27358f5bc00aeeabc2cae4e9483325cadcf24f
KazakovMaksim <kazakovmaksimi@gmail.com>
2021-03-03 19:12:29 +0300

    add scripts cats and team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,22 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+  
   // remove line with error and write your code here
+  let cat = "^^";
+  let count = 0;
+  let str;
+  let arr = [];
+  
+        for (let i = 0; i < arguments.length; i++){
+            arr = [];
+            str = arguments[i].join(",");
+            arr = str.split(",");
+          for (let j = 0; j < arr.length; j++){
+              if (arr[j] == cat) {
+                  count++;
+              }
+          }
+          
+        }
+        return count;
+
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,37 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function createDreamTeam(members) {
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  let arr = [];
+  let str;
+  let k = 0;
+        // console.log(members);
+        if (!Array.isArray(members)) {
+            return false; 
+          }
+          
+        for (i = 0; i < members.length; i++) {
+                           
+            if (typeof members[i] === "string") {
+               str = members[i];
+                  
+                  for (j = 0; j < str.length; j++){
+                    if (str[j] !== " ") {
+                     arr[k] = str[j];
+                     k++;
+                     break;
+                     }
+                    
+                  }
+               
+            }
+          
+        }
+  
+        str = arr.join("").toUpperCase();
+        arr = str.split('');
+        str = arr.sort().join("");
+          
+        return str.toUpperCase();
+  
+    // remove line with error and write your code here
+  };
+  
