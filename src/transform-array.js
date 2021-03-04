4b409557d83c25ad5037da9991664bce894fd714
KazakovMaksim <kazakovmaksimi@gmail.com>
2021-03-04 13:25:33 +0300

    feat: add script

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,42 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  let array = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    array[i] = arr[i];
+  } 
+
+if (array) {       
+for (let i = 0; i < array.length; i++) {
+           if (array[i] === "--discard-prev" && arr[i-1]) {
+             array.splice(i-1,2); 
+           }
+           if (array[i] === "--discard-prev") {
+             array.splice(i,1);
+           }
+            
+           if (arr[i] === "--discard-next" && arr[i+1] && ((arr[i+2] == "--discard-prev") || (arr[i+2] == "--double-prev"))) {
+            array.splice(i,3);
+           }
+
+           if (arr[i] === "--discard-next" && arr[i+1]) {
+               array.splice(i,2);
+          } if (arr[i] === "--discard-next") {
+               array.splice(i,1);
+            }
+  
+          if (arr[i] === "--double-next" && arr[i+1]) {
+              array.splice(i,1,array[i+1]);
+          } if (arr[i] === "--double-next") {
+              array.splice(i,1);
+            }
+          
+          if (array[i] === "--double-prev" && arr[i-1]) {
+              array.splice(i,1,array[i-1]);
+          } if (arr[i] === "--double-prev") {
+              array.splice(i,1)
+            }
+}
+} else return NaN;
+return array;
+   }
