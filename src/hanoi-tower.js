8b0c6d2eb6c9cf1ea696edd055b7d109ec676e3d
KazakovMaksim <kazakovmaksimi@gmail.com>
2021-03-21 22:09:03 +0300

    feat: add hanoi script

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,13 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  console.log(arguments);
+  arr = {};
+  arr["turns"] = Math.pow(2,disksNumber) - 1;
+  arr["seconds"] = Math.floor((Math.pow(2,disksNumber) - 1) / (turnsSpeed / 3600));
+  console.log(arr);
+  return arr;
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
 };
+// console.log(argument);
+
+
+// assert.deepEqual(calculateHanoi(9, 4308), {turns: 31, seconds: 27});
